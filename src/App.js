f6785dff791f5cf2c331a2ce16403e03aafe8ab7
psynewave <mark@pezops.com>
2015-11-07 09:01:43 -0800

    implemented auth0

--- src/App.js
+++ src/App.js
@@ -15,12 +15,70 @@ $.ajaxSetup({ cache: false });
 export default class App extends Component {
   constructor() {
     super();
+    this.state = {
+      copyYear: new Date().getFullYear(),
+      location : "94085",
+      appName: 'Research.ly'
+    };
+    this.getLock = this.getLock.bind(this);
+    this.showLock = this.showLock.bind(this);
+    this.getIdToken = this.getIdToken.bind(this);
+    this.getProfile = this.getProfile.bind(this);
+  }
+
+  showLock(){
+    this.lock.show();
+  }
+
+  getLock() {
+    this.lock = new Auth0Lock('GVLRAhDhK14MFwrjwzb9zSCAVzUC4xY7', 'waverunner.auth0.com');
+  }
+
+  getIdToken() {
+    let idToken = localStorage.getItem('userToken');
+    let authHash = this.lock.parseHash(window.location.hash);
+    if (!idToken && authHash) {
+      if (authHash.id_token) {
+        idToken = authHash.id_token
+        localStorage.setItem('userToken', authHash.id_token);
+      }
+      if (authHash.error) {
+        console.log("Error signing in", authHash);
+        return null;
+      }
+    }
+    return idToken;
+  }
+
+  getProfile(){
+    let token = this.getIdToken();
+    if(token){
+      this.lock.getProfile(token, function (err, profile) {
+        if (err) {
+          console.log("Error loading the Profile", err);
+        }
+        this.setState({
+          profile: profile
+        });
+      }.bind(this));
+    }
+  }
+
+  componentWillMount () {
+    this.getLock();
+    this.setState({
+      idToken: this.getIdToken()
+    });
+    this.getProfile();
   }
 
   render() {
 
     return (
-      <div>Hello World</div>
+      <div><a className="ui huge button positive colored" onClick={this.showLock}>
+                  <i className="dashboard icon"></i>
+                  Take a Test Drive
+                </a></div>
     );
   }
 }
