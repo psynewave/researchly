abe7b0f0baffd47558cc28bb11d8989d08b45eb8
ashantal <aantal@Engineering-MacBook-Pro.local>
2015-11-08 06:27:45 -0800

    simple parser

--- src/chat/Message.js
+++ src/chat/Message.js
@@ -1,13 +1,20 @@
 import React  from 'react/addons';
+import Parser from 'simple-text-parser';
 export default class Message extends React.Component {
   render(){
     let props = this.props;
+    var p = new Parser();
+    p.addRule(/\#[\S]+/ig, function(tag) {
+        return '<a class="ui green label">'+ tag.substr(1) +'</a>';
+    });
+    let message =  p.parse(props.thread.title);
+
     return (
       <div className='comment' onClick={ props.handleClick.bind(null) } className={ props.show ? 'bg-warning' : 'bg-info'}>
 
               <img className="ui avatar image" src={props.thread.avatar} alt={props.thread.name} />
               <div className="ui segment left pointing label">
-                { props.thread.title }
+                <div dangerouslySetInnerHTML={{__html: message}} />
                 <i onClick={ props.removeMessage.bind(null) } className="close icon"></i>
              </div>
       </div>
