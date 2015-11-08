42bcab1f219b95497c1b7c11e305f373aa6af2bd
psynewave <mark@pezops.com>
2015-11-07 16:21:20 -0800

    fixed the nightmare that was jquery ui styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import 'imports?$=jquery,jQuery=jquery!../css/semantic/semantic.min.js';
-import 'imports?$=jquery,jQuery=jquery!../css/jquery-ui/jquery-ui.js';
-
 import '../css/style.css';
 import React, { Component } from 'react';
 import Actions from './actions/appActions';
